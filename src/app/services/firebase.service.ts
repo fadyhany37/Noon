5bf4174ba228fc77da4197036d66d68313c96aaa
Alaa-Ezzat <alaa.awadallah97@gmail.com>
2022-07-15 15:00:36 +0200

    adding bootstrap to package-lock

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,36 +1,28 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore} from '@angular/fire/compat/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseService {
+  constructor(private store: AngularFirestore) {}
 
-  constructor(private store: AngularFirestore) { }
-
-
-  getproducts(){
-
-    return this.store.collection("Products").snapshotChanges();
-
+  getproducts() {
+    return this.store.collection('Products').snapshotChanges();
   }
 
-  addproduct(product:any) {
-
-    this.store.collection("Products").add(product);
+  addproduct(product: any) {
+    this.store.collection('Products').add(product);
   }
 
-  deleteproduct(pId:string) {
-
-    this.store.collection("Products").doc(pId).delete();
+  deleteproduct(pId: string) {
+    this.store.collection('Products').doc(pId).delete();
   }
-  
-  updateproduct(pId:string,product:any) {
 
-    this.store.collection("Products").doc(pId).set(product);
+  updateproduct(pId: string, product: any) {
+    this.store.collection('Products').doc(pId).set(product);
   }
 
   getProductById(pId: string) {
-    return this.store.collection("Products").doc(pId).snapshotChanges();
+    return this.store.collection('Products').doc(pId).snapshotChanges();
   }
 }
-
