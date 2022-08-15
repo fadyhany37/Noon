b238e226043cf504638887ba93d852fd6ba64e16
fadyhany37 <fadyhany37@gmail.com>
2022-08-15 09:28:58 +0200

    merge cartoperaiton

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -43,6 +43,7 @@ export class CartService {
         this.cartItemList.splice(index, 1)
       }
     })
+    localStorage.setItem("cart", JSON.stringify(this.cartItemList))
     this.productList.next(this.cartItemList)
   }
 }
